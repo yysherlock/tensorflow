4009851cdd62be6ce092a642b9a18f6f6e223fc2
Zhiyi Luo <jessieluo1991@gmail.com>
2016-02-23 22:15:40 +0800

    update notmnist.py

--- notebook/notmnist.py
+++ notebook/notmnist.py
@@ -54,3 +54,137 @@ def maybe_extract(filename, force = False):
 
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
+
+image_size = 28       # Pixel width and height
+pixel_depth = 255.0   # Number of levels per Pixel
+
+def load_letter(folder, min_num_images):
+    """ Load the data for a single letter label. """
+    image_files = os.listdir(folder)
+    dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                dtype=np.float32) # initialized np.ndarray
+
+    image_index = 0
+    print(folder)
+
+    for image in os.listdir(folder):
+        image_file = os.path.join(folder, image)
+        try:
+            # normalize: approximately zero mean and standard deviation ~0.5
+            image_data = (ndimage.imread(image_file).astype(float) - pixel_depth / 2) / pixel_depth
+            if image_data.shape != (image_size, image_size):
+                raise Exception('Uxpected image shape %s' % str(image_data.shape))
+            dataset[image_index,:,:] = image_data
+            image_index += 1
+
+        except IOError as e:
+            print('Could not read:', image_file, ':', e, '-It\'s ok, skipping')
+
+    num_images = image_index
+    dataset = dataset[0:num_images,:,:]
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' % (num_images, min_num_images))
+    print('Full dataset tensor:', dataset.shape)
+    print('Mean:', np.mean(dataset))
+    print('Standar deviation:', np.std(dataset))
+
+    return dataset
+
+def maybe_pickle(data_folders, min_num_images_per_class, force = False):
+    dataset_names = []
+    for folder in data_folders:
+        set_filename = folder + '.pickle'
+        dataset_names.append(set_filename)
+        if os.path.exists(set_filename) and not force:
+            print('%s already present - Skipping pickling.' % set_filename)
+        else:
+            print('Pickling %s.' % set_filename)
+            dataset = load_letter(folder, min_num_images_per_class)
+            try:
+                with open(set_filename,'wb') as f:
+                    pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Unable to save data to', set_filename, ':', e)
+    return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+def make_arrays(nb_rows, img_size):
+    if nb_rows:
+        dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+        labels = np.ndarray(nb_rows, dtype=np.int32)
+    else:
+        dataset, labels = None, None
+    return datset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size = 0):
+    num_classes = len(pickle_files)
+    valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+    train_dataset, train_labels = make_arrays(train_size, image_size)
+    vsize_per_class = valid_size // num_classes
+    tsize_per_clas = train_size // num_classes
+
+    start_v, start_t = 0,0
+    end_v, end_t = vsize_per_class, tsize_per_class
+    end_l = vsize_per_class + tsize_per_class
+
+    for label, pickle_file in enumerate(pickle_files):
+        try:
+            with open(pickle_file,'rb') as f:
+                letter_set = pickle.load(f)
+                # let's shuffle the letters to have random validation and training setting
+                np.random.shuffle(letter_set)
+                if valid_dataset is not None:
+                    valid_letter = letter_set[:vsize_per_class,:,:]
+                    valid_dataset[start_v:end_v,:,:] = valid_letter
+                    valid_labels[start_v:end_v] = label
+                    start_v += vsize_per_class
+                    end_v += vsize_per_class
+
+                train_letter = letter_set[vsize_per_class:end_l,:,:]
+                train_dataset[start_t:end_t,:,:] = train_letter
+                train_labels[start_t:end_t] = label
+                start_t += tsize_per_class
+                end_t += tsize_per_class
+        except Exception as e:
+            print('Unable to process data from', pickle_file,':',e)
+            raise
+    return valid_dataset, valid_labels, train_dataset, train_labels
+
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
+valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(train_datasets, train_size, valid_size)
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
+
+def randomize(dataset, labels):
+    permutation = np.random.permutation(labels.shape[0])
+    shuffled_dataset = dataset[permutatipon,:,:]
+    shuffled_labels = labels[permutation]
+    return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+pickle_file = 'notMNIST.pickle'
+try:
+    f = open(pickle_file, 'wb')
+    save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+    pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+    f.close()
+except Exception as e:
+    print('Unable to save data to', pickle_file, ':', e)
+    raise
